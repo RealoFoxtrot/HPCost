3c3f0dd6551d154176e3a7894d903859ebde04c9
Wester Coenraads <wester.coenraads@gmail.com>
2017-07-15 16:22:29 +0200

    Add skeleton creature class.

---
+++ DualMechanics/Assets/Scripts/Creature.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    private const string PrefabPath = "Creature";
+
+    public int Health { get; private set; }
+    public bool PlayerController { get; private set; }
+
+    public static void Spawn()
+    {
+        GameObject creature_obj = GameObject.Instantiate(QuickLoad.Load<GameObject>(PrefabPath));
+        // TODO: give it a good position in the scene.
+    }
+
+    public void OnDrawGizmos()
+    {
+        // No sprite attached at the moment, so just draw a gizmo on our position.
+        Gizmos.DrawWireSphere(this.transform.position, 1f);
+    }
+}
